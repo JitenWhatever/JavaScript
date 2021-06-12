dffea81851193c325ec7fdb653443d27895f782a
jitenwhatever <jitendra1551k@gmail.com>
2021-06-13 01:24:27 +0530

    webpack

--- Drag & Drop/src/components/project-list.ts
+++ Drag & Drop/src/components/project-list.ts
@@ -1,9 +1,9 @@
-import { Component } from "./base-component.js";
-import { DragTarget } from "../models/drag-drop.js";
-import { projectState } from "../state/project.js";
-import { Autobind } from "../decorators/autobind.js";
-import { Project, ProjectStatus } from "../models/project.js";
-import { ProjectItem } from "./project-item.js";
+import { Component } from "./base-component";
+import { DragTarget } from "../models/drag-drop";
+import { projectState } from "../state/project-state";
+import { Autobind } from "../decorators/autobind";
+import { Project, ProjectStatus } from "../models/project";
+import { ProjectItem } from "./project-item";
 
 export class ProjectList
   extends Component<HTMLDivElement, HTMLElement>
