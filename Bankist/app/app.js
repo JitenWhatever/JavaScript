d97da29421489add2ddbf43ed0ce521f70e2df2a
Jitendra Kumar <jitendra1551k@gmail.com>
2022-05-01 02:20:24 +0530

    containerisation of bankist app

---
+++ Bankist/app/app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+
+app.use(express.static(path.join(__dirname, "public")));
+app.use("/images", express.static(path.join(__dirname, "images")));
+
+
+app.set("view engine", "ejs");
+app.set("views", "views");
+
+
+app.get("/bank", function(req, res) {
+    res.status(200).render('index')
+});
+
+app.get("/", function(req, res) {
+    res.status(200).render('index')
+});
+
+app.listen(8080)
--- Bankist/app/public/js/script.js
+++ Bankist/app/public/js/script.js
@@ -204,6 +204,7 @@ let currentAccount, timer;
 
 btnLogin.addEventListener('click', function (event) {
   event.preventDefault();
+  console.log("click happend on login")
   currentAccount = accounts.find(
     account => account.username === inputLoginUsername.value
   );
